58673d5fbddcec7009f9c2ec70e8c1e48f49d025
eko setiadi <exaudie@gmail.com>
2023-02-16 21:57:01 +0700

    feat(main): new poject

---
+++ lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:task_list/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:task_list/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:task_list/ui/views/home/home_view.dart';
+import 'package:task_list/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+    // @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
---
+++ test/viewmodels/notice_sheet_model_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:task_list/app/app.locator.dart';
+
+import '../helpers/test_helpers.dart';
+
+void main() {
+  group('InfoAlertDialogModel Tests -', () {
+    setUp(() => registerServices());
+    tearDown(() => locator.reset());
+  });
+}
